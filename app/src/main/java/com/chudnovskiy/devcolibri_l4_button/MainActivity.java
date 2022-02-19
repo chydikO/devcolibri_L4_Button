942c4e950e1c0337278522f9fde84f6cdc53716f
Chudnovskiy.O.W520 <oleg.chudnovskiy@gmail.com>
2022-02-19 10:15:05 +0200

    Initial commit

---
+++ app/src/main/java/com/chudnovskiy/devcolibri_l4_button/MainActivity.java
@@ -0,0 +1,49 @@
+package com.chudnovskiy.devcolibri_l4_button;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private Button button;
+    private Button button2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        hideNavigationBar();
+        getSupportActionBar().hide();
+
+        textView = (TextView) findViewById(R.id.text_vew);
+        button = (Button) findViewById(R.id.button);
+        button2 = (Button) findViewById(R.id.button2);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setText("setOnClickListener");
+            }
+        });
+    }
+
+    public void btnClick(View view) {
+        textView.setText(getResources().getText(R.string.hello_world));
+    }
+
+    private void hideNavigationBar() {
+        View decorView = getWindow().getDecorView();
+// Hide both the navigation bar and the status bar.
+// SYSTEM_UI_FLAG_FULLSCREEN is only available on Android 4.1 and higher, but as
+// a general rule, you should design your app to hide the status bar whenever you
+// hide the navigation bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+
+    }
+}
